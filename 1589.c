019e07826b3ba4e93be2ffca845712d3677bd406
CaioCarvalh <caiocarvalhordo@gmail.com>
2025-07-11 19:09:40 -0300

    Create 1589.c

---
+++ 1589.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    int R1, R2;
+
+    scanf("%d", &T);
+
+    while (T--) {
+        scanf("%d %d", &R1, &R2);
+        printf("%d\n", R1 + R2);
+    }
+
+    return 0;
+}
